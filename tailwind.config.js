ac71f652df26477af995be95ffd10a02679a687d
Martijn Dorsman <martijn.dorsman@gmail.com>
2023-08-29 21:29:39 +0200

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
-  mode: 'jit',
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        dark: '#121212',
-        'dark-light': '#272727',
-        'dark-lighter': '#444',
-      }
+        dark: "#121212",
+        "dark-light": "#272727",
+        "dark-lighter": "#444",
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
